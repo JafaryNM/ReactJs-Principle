7da94df355c687d2360b7c50d3fa4a4955d6c49d
JafaryNm <jaffarymdegela@gmail.com>
2022-05-28 19:20:54 +0300

    Set State In Advance

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 
 import './App.css';
-import Test from './Components/Test';
-import Hello from './Components/Hello';
-import Message from './Components/Message';
+import Counter from './Components/Counter';
+
 
 function App() {
   return (
     <div className="App">
-
-      <Message></Message>
-
-   
-
-      <Test name= "Regina"></Test>
-      <Test name="Lucy"></Test>
-      <Test name="Juliana"></Test>
      
-     <Hello name=" Jafary" heroName="Engineer">
-       <p>
-         This is children paragraph
-       </p>
-     </Hello>
-     <Hello name=" Lucy"  heroName="Developer">
-       
-       <button> Action</button>
-         
-     </Hello>
-     <Hello name= " Faith " heroName="Front-end Developer"></Hello>
+     <Counter></Counter>
 
       
     
