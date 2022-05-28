ad48d861684c0f3e26ef423be83c03f598ad1edd
JafaryNm <jaffarymdegela@gmail.com>
2022-05-28 14:58:06 +0300

    React JSX Concept

---
+++ src/Components/Test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function test() {
+  return (
+    <div>
+      <h1> Test about react skills</h1>
+      <p>
+          this is paragraph test
+      </p>
+
+    </div>
+  )
+}
+
+export default test
